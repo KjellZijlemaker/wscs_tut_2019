f2f15e488e9ba324f7fda9cf48ab34757975f542
shaw-rohit <shaw.rohit@gmail.com>
2019-04-26 11:43:25 +0200

    Updated files

--- Assignment2/part2.1/update_html.py
+++ Assignment2/part2.1/update_html.py
@@ -1,11 +1,13 @@
 import fileinput
 
 with open('IP.txt', 'r') as file:
-    ip = file.read()
+        ip = file.read().replace('\n', '').replace(' ', '')
+
 f = open('temp.html', 'r')
 content = f.read()
 
-new_content = content.replace("host_name", "http://" + ip + ":5003/")
+new_content = content.replace("host_name", "http://" + ip + ":5002/")
 
 f = open('index.html', 'w')
 f.write(new_content)
+
